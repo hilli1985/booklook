424a3214750d6116107d008ad5d4eec3f5f31e22
Hilla Meir <hillameir85@gmail.com>
2018-08-18 15:36:10 +0300

    after code review 1

--- public/script.js
+++ public/script.js
@@ -1,24 +1,26 @@
-let datax;
+let dataBook;
 let items;
 let item;
-let BOARDER10 = 10;
 
-var source = document.getElementById("entry-template").innerHTML;
+var source = $("#entry-template").html();
 var template = Handlebars.compile(source);
 
-let render = function() {
+let render = function(select) {
+    //render in case of error
     $('.books').empty();
+    if (select==='select'){
+        $('.books').append("Sorry You Have To Select First");
+        return;
+    }
     $('.books').append("<h3 class='loading'>" + "Loading..." + "</h3>");
-    items = datax.items;
+    items = dataBook.items;
     if (!items) {
         $('.loading').remove();
         $('.books').append("<h3 class='not found'>" + "Sorry Notihng Found..." + "</h3>");
         return;
     }
-    if (items.length <= 10) {
-        BOARDER10 = items.length;
-    }
-    for (let i = 0; i < BOARDER10; i++) {
+    // render results
+    for (let i = 0; i < 10; i++) {
         item = items[i].volumeInfo;
         let title = item.title;
         let description = item.description;
@@ -31,87 +33,66 @@ let render = function() {
             if (item.imageLinks) {
                 image = item.imageLinks.thumbnail;
             } else
-                image = '';
-            var context = { title: title, description: description, authors: authors, image: image };
-            var html = template(context);
-            $('.loading').remove();
-            $('.books').append(html);
+            image = '';
+            var context = { title: title, 
+                description: description, 
+                authors: authors, 
+                image: image };
+                var html = template(context);
+                $('.loading').remove();
+                $('.books').append(html);
+            }
         }
     }
-}
+    
+    let _searchItemByTitle =  function(title){
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i].volumeInfo;
+            if (item.title===title){
+                var context = { title: item.title, 
+                    description: item.description, 
+                    authors: item.authors[0], 
+                    image: item.imageLinks.thumbnail };
+                return context;
+                } 
+            }
+        };
+        
+        let renderSpecificBook = function(title) {
+            var context = _searchItemByTitle(title);
+            console.log(context);
+            var html = template(context);
+            $('.books').html(html);
+        };
+        
+        //q=isbn:,q=intitle:,q=inauthor:
+        var fetchBySelect = function(select,lookup_value){
+            let url = 'https://www.googleapis.com/books/v1/volumes?q='+select+':'+lookup_value;
+            $.ajax({
+                method: "GET",
+                url: 'https://www.googleapis.com/books/v1/volumes?q='+select+':'+lookup_value,
+                success: function(data) {
+                    dataBook = data;
+                    render(select);
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus);
+                }
+            });
+        };
+        
+        //Events 
 
-
-let renderSpecificBook = function(context) {
-    $('.books').empty();
-    var context = context;
-    var html = template(context);
-    $('.books').html(html);
-}
-
-
-$('.form-isbn').on('click', '.search-btn', function() {
-    let isbn = $('.form-isbn').find('#book-isbn').val();
-    fetchByISBN(isbn);
-});
-var fetchByISBN = function(isbn) {
-    $.ajax({
-        method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
-        success: function(data) {
-            datax = data;
-            render();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus);
-        }
-    });
-};
-
-var fetchByAuthor = function(author) {
-    console.log(author);
-    $.ajax({
-        method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=inauthor:' + author,
-        success: function(data) {
-            datax = data;
-            render();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus);
-        }
-    });
-};
-
-$('.form-title').on('click', '.search-btn', function() {
-    let title = $('.form-title').find('#book-title').val();
-    fetchByTitle(title);
-});
-
-$('.form-author').on('click', '.search-btn', function() {
-    let author = $('.form-author').find('#book-author').val();
-    fetchByAuthor(author);
-});
-
-var fetchByTitle = function(title) {
-    $.ajax({
-        method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + title,
-        success: function(data) {
-            datax = data;
-            render();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus);
-        }
-    });
-};
-
-
-$('.books').on('click', '.img-btn', function() {
-    let title = ($(this).closest(".book").children('h3').text());
-    let authors = ($(this).closest(".book").children('h4').text().replace('Written By:', ''));
-    let description = ($(this).closest(".book").children('p').text());
-    let image = ($(this).css("background-image").replace('url', '').replace('(', '').replace(')', ''));
-    let context = { title: title, description: description, authors: authors, image: image };
-    renderSpecificBook(context);
-});
+        $('.form-search').on('click', '.search-btn', function() {
+            let select = $( "#select option:selected" ).val();
+            console.log("selcet:"+select);
+            let lookup_value = $('.form-search').find('#lookup-value').val();
+            console.log("lookup_value:"+lookup_value);
+            fetchBySelect(select,lookup_value);
+        });
+        
+        $('.books').on('click', '.img-btn', function() {
+            let title = ($(this).closest(".book").children('h3').text());
+            console.log(title);
+            renderSpecificBook(title);
+        });
